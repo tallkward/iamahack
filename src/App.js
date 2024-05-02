b5b7bd080a5572d2b3f06fc274a7b24c8f23a3a6
A.J. Reeves <tallkward@gmail.com>
2024-05-02 15:52:32 -0500

    first commit :)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './navbar.js'; 
+import Footer from './footer.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NavBar /> 
+
+      <main role="main">
+        <div className="mt-5">
+          <h1>Main Content Area</h1>
+          <p>This is your main content area. You can put your content here.</p>
+        </div>
+      </main>
+     <Footer />
     </div>
   );
 }
